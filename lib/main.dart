79e2b9fb4c9197ceab715c35a1dc57ed5fbf0e8d
sabrisninha <sabrina.gomes2@estudante.ifms.edu.br>
2023-04-18 13:50:39 -0400

    lalala

--- lib/main.dart
+++ lib/main.dart
@@ -20,13 +20,28 @@ class PaginaInicial extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Titulo do app"),
+        title: Text("Meu app"),
         backgroundColor: Colors.purple,
       ),
+      drawer: Drawer(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("menu"),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text("botãozão"),
+          )
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.favorite),
+        onPressed: () {},
+      ),
       body: Container(
-          color: Color.fromARGB(255, 238, 165, 160),
-          height: 200,
           alignment: Alignment.center,
+          color: Color.fromARGB(255, 238, 165, 160),
+          height: 1000,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
